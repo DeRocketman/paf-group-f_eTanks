76df86ef84107ff31e2a31f9a1e5dd9b8133940d
Dirk Stricker <dirk.stricker@stud.th-lubeck.de>
2021-11-05 22:45:39 +0100

    DS: add Game Class

---
+++ client_jfx/src/main/java/model/Game.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public class Game {
+    private final int id;
+    private final Timestamp gameTimestamp;
+    private final User host;
+    private final ArrayList<User> participants;
+    private ArrayList<UserGameStatistic> userGameStatistics;
+
+    public Game(int id, User host, ArrayList<User> participants) {
+        this.id = id;
+        this.host = host;
+        this.participants = participants;
+        this.gameTimestamp = new Timestamp(System.currentTimeMillis());
+        drawLevel();
+        createUserGameStatistic();
+    }
+
+    public void drawLevel() {
+        //todo: ausformulieren des Methodenrumpfes drawLevel (wirklich hier? oder in view?
+    }
+
+    public void createUserGameStatistic() {
+        int playerCount = participants.size() + 1;
+        for (int i = 0; i < playerCount; i++) {
+            UserGameStatistic userGameStatistic = new UserGameStatistic(0,0,0,0,0);
+            this.userGameStatistics.add(userGameStatistic);
+        }
+    }
+}

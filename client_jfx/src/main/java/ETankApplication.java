b17f5bc800095eb17c9a84e22021d653fdf1268a
Maximiliami <maximilian.rupprecht@stud.th-luebeck.de>
2021-11-08 19:34:52 +0100

    MOB: Level 1

--- client_jfx/src/main/java/ETankApplication.java
+++ client_jfx/src/main/java/ETankApplication.java
@@ -1,4 +1,8 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ETankApplication extends Application {
@@ -9,6 +13,16 @@ public class ETankApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ETankApplication.class.getResource("view/LevelOneView.fxml"));
+            AnchorPane rootLayout = (AnchorPane) loader.load();
 
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }

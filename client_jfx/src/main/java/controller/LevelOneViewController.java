67d5ae4de7dae30517ec4d701b6bb312c537b7e6
Maximiliami <maximilian.rupprecht@stud.th-luebeck.de>
2021-11-08 20:37:03 +0100

    MOB: Controller angelegt

---
+++ client_jfx/src/main/java/controller/LevelOneViewController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+
+public class LevelOneViewController {
+
+    @FXML
+    private Group tankPlayerOne = new Group();
+
+}

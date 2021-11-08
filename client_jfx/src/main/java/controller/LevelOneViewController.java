57a370c637437d836300d6a01c45e37e8145adaa
Dirk Stricker <dirk.stricker@stud.th-luebeck.de>
2021-11-08 21:40:02 +0100

    Pair-Programming: The tank is moving now :P

--- client_jfx/src/main/java/controller/LevelOneViewController.java
+++ client_jfx/src/main/java/controller/LevelOneViewController.java
@@ -2,10 +2,26 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+
 
 public class LevelOneViewController {
 
     @FXML
-    private Group tankPlayerOne = new Group();
+    public Group tankOne;
 
+
+    @FXML
+    public void moveUPClicked(){
+           tankOne.setLayoutX(450.0);
+           tankOne.setLayoutY(30.0);
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Geklickt");
+
+        alert.showAndWait();
+    }
 }

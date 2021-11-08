0be53e1f5610b872ab935b92dbb314b08376ac09
Dirk Stricker <dirk.stricker@stud.th-luebeck.de>
2021-11-08 15:55:52 +0100

    DS: build 3 level and edit UserGameStatistic

--- client_jfx/src/main/java/model/UserGameStatistic.java
+++ client_jfx/src/main/java/model/UserGameStatistic.java
@@ -1,8 +1,11 @@
 package model;
 
-//todo: Klaeren, ob noch winningGames and winningRounds dazukommen sollen DS= ja
+//todo: Klaeren, ob noch gameWins and roundWins dazukommen sollen DS= ja
 public class UserGameStatistic {
     private int gamePoints;
+    private int playedGames;
+    private int gameWins;
+    private int roundWins;
     private int kills;
     private int deaths;
     private int shots;
@@ -11,70 +14,34 @@ public class UserGameStatistic {
     private float hitRate;
 
     public UserGameStatistic() {
+
+    }
+
+    public UserGameStatistic(int gamePoints, int playedGames, int gameWins, int roundWins, int kills, int deaths, int shots, int hitPoints) {
         this.gamePoints = gamePoints;
+        this.playedGames = playedGames;
+        this.gameWins = gameWins;
+        this.roundWins = roundWins;
         this.kills = kills;
         this.deaths = deaths;
         this.shots = shots;
         this.hitPoints = hitPoints;
+
+        this.killDeathRate = killDeathRate();
+        this.hitRate = hitRate();
+
     }
 
-    public void killDeathRate() {
+
+    public float killDeathRate() {
         if (this.deaths > 0) {
-            this.killDeathRate = this.kills/this.deaths;
+            return this.kills / this.deaths;
         } else {
-            this.killDeathRate= this.kills;
+            return this.kills;
         }
     }
 
-    public void hitRate() {
-        this.hitRate = hitPoints/shots;
-    }
-
-    public int getGamePoints() {
-        return gamePoints;
-    }
-
-    public void setGamePoints(int gamePoints) {
-        this.gamePoints = gamePoints;
-    }
-
-    public int getKills() {
-        return kills;
-    }
-
-    public void setKills(int kills) {
-        this.kills = kills;
-    }
-
-    public int getDeaths() {
-        return deaths;
-    }
-
-    public void setDeaths(int deaths) {
-        this.deaths = deaths;
-    }
-
-    public int getShots() {
-        return shots;
-    }
-
-    public void setShots(int shots) {
-        this.shots = shots;
-    }
-
-    public int getHitPoints() {
-        return hitPoints;
-    }
-
-    public void setHitPoints(int hitPoints) {
-        this.hitPoints = hitPoints;
-    }
-
-    public float getKillDeathRate() {
-        return killDeathRate;
-    }
-
-    public float getHitRate() {
-        return hitRate;
+    public float hitRate() {
+        return hitPoints / shots;
     }
 }

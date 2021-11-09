956f82213191edf0a75c120e113f3eb381e13223
Maximiliami <maximilian.rupprecht@stud.th-luebeck.de>
2021-11-09 19:28:21 +0100

    Login.fxml noch keine Funktion

--- client_jfx/src/main/java/controller/LoginViewController.java
+++ client_jfx/src/main/java/controller/LoginViewController.java
@@ -1,4 +1,26 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginViewController {
+
+    @FXML
+    TextField UsernameField;
+    @FXML
+    PasswordField pwField;
+    @FXML
+    Label forgotPwLabel;
+    @FXML
+    Label newUserLabel;
+    @FXML
+    Button loginBtn;
+
+
+
+
+
 }

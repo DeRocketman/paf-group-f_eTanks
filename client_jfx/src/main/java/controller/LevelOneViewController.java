d293716818f643f85296257090c61faac7345486
Dirk Stricker <dirk.stricker@stud.th-luebeck.de>
2021-11-22 17:09:08 +0100

    DS: better tankemovement but not perfect!

--- client_jfx/src/main/java/controller/LevelOneViewController.java
+++ client_jfx/src/main/java/controller/LevelOneViewController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javafx.animation.Animation;
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
@@ -7,6 +9,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.RotateEvent;
+import javafx.util.Duration;
 import main.ETankApplication;
 
 import java.awt.event.ActionEvent;
@@ -18,6 +22,7 @@ import java.util.ResourceBundle;
 public class LevelOneViewController implements Initializable {
 
     ETankApplication eTankApplication;
+    boolean allowInput = true;
 
     @FXML
     public Group tankOne;
@@ -32,16 +37,45 @@ public class LevelOneViewController implements Initializable {
     public Group tankFour;
 
     public void keyPressed(KeyEvent keyEvent) {
-        if (keyEvent.getCode() == KeyCode.W) {
-            tankOne.setLayoutY(tankOne.getLayoutY()-10.0);
-        }
-        if (keyEvent.getCode() == KeyCode.S) {
-            tankOne.setLayoutY(tankOne.getLayoutY()+10.0);
-        }
-        if (keyEvent.getCode() == KeyCode.D) {
+            Group myTank = tankOne;
+            if (keyEvent.getCode() == KeyCode.W) {
+                moveTank(myTank, 0.0);
+            }
+            if (keyEvent.getCode() == KeyCode.S) {
+                moveTank(myTank, 180.0);
+            }
+            if (keyEvent.getCode() == KeyCode.D) {
+                moveTank(myTank, 90.0);
+            }
+            if (keyEvent.getCode() == KeyCode.A) {
+                moveTank(myTank, 270.0);
+            }
+    }
 
+    public void moveTank(Group myTank, double newCourse) {
+        double speed = 10.0;
+        RotateTransition rt = new RotateTransition(Duration.seconds(0.2), myTank);
+        rt.setFromAngle(myTank.getRotate());
+        rt.setToAngle(newCourse);
+        if(rt.getStatus() != Animation.Status.RUNNING) {
+            if(myTank.getRotate() == newCourse) {
+                if(newCourse==0.0) {
+                    myTank.setLayoutY(myTank.getLayoutY()-speed);
+                } else if (newCourse==180.0) {
+                    myTank.setLayoutY(myTank.getLayoutY()+speed);
+                } else if (newCourse==90.0) {
+                    myTank.setLayoutX(myTank.getLayoutX()+speed);
+                } else if (newCourse==270.0) {
+                    myTank.setLayoutX(myTank.getLayoutX()-speed);
+                }
+            } else {
+                rt.play();
+
+            }
         }
     }
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {

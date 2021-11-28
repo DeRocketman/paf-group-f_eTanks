92edf2cdc5d92562ac6d6d5287ceb5af539e9143
Maximiliami <maximilian.rupprecht@stud.th-luebeck.de>
2021-11-28 12:16:32 +0100

    RESTAPI

---
+++ ETankapplicationRESTAPI/ETankRESTAPI/src/main/java/thl/gruppef/etankrest/etankrestapi/ETankRestapiApplication.java
@@ -0,0 +1,13 @@
+package thl.gruppef.etankrest.etankrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETankRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETankRestapiApplication.class, args);
+    }
+
+}
---
+++ ETankapplicationRESTAPI/ETankRESTAPI/src/test/java/thl/gruppef/etankrest/etankrestapi/ETankRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package thl.gruppef.etankrest.etankrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETankRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

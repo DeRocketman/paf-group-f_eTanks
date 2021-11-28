2862f4657ace090b701d0a1a830432cb19ecae7d
Maximiliami <maximilian.rupprecht@stud.th-luebeck.de>
2021-11-28 18:12:29 +0100

    REST API

--- ETankapplicationRESTAPI/ETankRESTAPI/src/main/java/thl/gruppef/etankrest/etankrestapi/controller/UserGameStatisticController.java
+++ ETankapplicationRESTAPI/ETankRESTAPI/src/main/java/thl/gruppef/etankrest/etankrestapi/controller/UserGameStatisticController.java
@@ -1,8 +1,13 @@
 package thl.gruppef.etankrest.etankrestapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import thl.gruppef.etankrest.etankrestapi.entities.User;
+import thl.gruppef.etankrest.etankrestapi.entities.UserGameStatistic;
 import thl.gruppef.etankrest.etankrestapi.repository.UserGameStatisticRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user_game_statistic")
@@ -12,4 +17,10 @@ public class UserGameStatisticController {
     public UserGameStatisticController(UserGameStatisticRepository userGameStatisticRepository) {
         this.userGameStatisticRepository = userGameStatisticRepository;
     }
+
+    @GetMapping("")
+    public List<UserGameStatistic> index() {
+        return userGameStatisticRepository.findAll();
+    }
+
 }
--- ETankapplicationRESTAPI/ETankRESTAPI/src/main/java/thl/gruppef/etankrest/etankrestapi/controller/UserSettingsController.java
+++ ETankapplicationRESTAPI/ETankRESTAPI/src/main/java/thl/gruppef/etankrest/etankrestapi/controller/UserSettingsController.java
@@ -1,10 +1,14 @@
 package thl.gruppef.etankrest.etankrestapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import thl.gruppef.etankrest.etankrestapi.entities.UserGameStatistic;
+import thl.gruppef.etankrest.etankrestapi.entities.UserSettings;
 import thl.gruppef.etankrest.etankrestapi.repository.UserSettingsRepository;
 
 import javax.persistence.OneToOne;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user_settings")
@@ -16,5 +20,8 @@ public class UserSettingsController {
         this.userSettingsRepository = userSettingsRepository;
     }
 
-
+    @GetMapping("")
+    public List<UserSettings> index() {
+        return userSettingsRepository.findAll();
+    }
 }

76a4fc83290b962414476da63a0845a8cb4eb338
DeRocketman <de.rocketman@outlook.com>
2021-12-19 23:56:16 +0100

    DS: build python loginView and much more

--- client_python/src/main/ETanksApp.py
+++ client_python/src/main/ETanksApp.py
@@ -1,1 +1,25 @@
+import sys
 
+
+from PySide6.QtCore import QFile, QIODevice
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+
+
+class ETankApp:
+
+    def __init__(self):
+        app = QApplication(sys.argv)
+        loginUI = QFile("../resources/view/loginView.ui")
+        if not loginUI.open(QIODevice.ReadOnly):
+            sys.exit(-1)
+        loader = QUiLoader()
+        window = loader.load(loginUI)
+
+        if not window:
+            sys.exit(-1)
+        window.show()
+        sys.exit(app.exec())
+
+if __name__ == '__main__':
+    app = ETankApp()

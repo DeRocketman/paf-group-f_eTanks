c216935a02286e3344126bec44d3da03fcbb40f7
DeRocketman <de.rocketman@outlook.com>
2021-12-28 17:29:58 +0100

    DS: Rollback and fixes MainMenuViewController.py again. Try load default picture

--- client_python/src/controller/SettingsViewController.py
+++ client_python/src/controller/SettingsViewController.py
@@ -1,11 +1,14 @@
 from enum import Enum
 
+import PySide6
+import keyboard as keyboard
+from PySide6 import QtGui
+from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QWidget
 
 from model.data.UserSettings import UserSettings
 from model.service.HttpRequest import HttpRequest
 from resources.view.SettingsView import Ui_settingsView
-import keyboard
 
 
 class SettingsViewController(QWidget):
@@ -20,21 +23,21 @@ class SettingsViewController(QWidget):
 
         self.settingsView.musicVolSlider.setValue(self.mainMenuController.signedUser.userSettings.gameMusicVolume)
         self.settingsView.soundVolSlider.setValue(self.mainMenuController.signedUser.userSettings.gameSoundVolume)
-        self.setValue(ButtonType.SOUND_ON)
-        self.setValue(ButtonType.MUSIC_ON)
-        self.setValue(ButtonType.MOVE_UP)
-        self.setValue(ButtonType.MOVE_DOWN)
-        self.setValue(ButtonType.MOVE_LEFT)
-        self.setValue(ButtonType.MOVE_RIGHT)
-        self.setValue(ButtonType.FIRE_MAIN)
-        self.setValue(ButtonType.FIRE_SECONDARY)
+        self.setInitValue(ButtonType.SOUND_ON)
+        self.setInitValue(ButtonType.MUSIC_ON)
+        self.setInitValue(ButtonType.MOVE_UP)
+        self.setInitValue(ButtonType.MOVE_DOWN)
+        self.setInitValue(ButtonType.MOVE_LEFT)
+        self.setInitValue(ButtonType.MOVE_RIGHT)
+        self.setInitValue(ButtonType.FIRE_MAIN)
+        self.setInitValue(ButtonType.FIRE_SECONDARY)
 
-        self.settingsView.moveUpButton.clicked.connect(self.changeKey)
-        self.settingsView.moveDownButton.clicked.connect(self.changeKey)
-        self.settingsView.moveLeftButton.clicked.connect(self.changeKey)
-        self.settingsView.moveRightButton.clicked.connect(self.changeKey)
-        self.settingsView.mainWeaponButton.clicked.connect(self.changeKey)
-        self.settingsView.secondaryWeaponButton.clicked.connect(self.changeKey)
+        self.settingsView.moveUpButton.clicked.connect(self.changeMoveUpKey)
+        self.settingsView.moveDownButton.clicked.connect(self.changeMoveDownKey())
+        self.settingsView.moveLeftButton.clicked.connect(self.changeMoveLeftKey())
+        self.settingsView.moveRightButton.clicked.connect(self.changeMoveRightKey())
+        self.settingsView.mainWeaponButton.clicked.connect(self.changeFireMainKey())
+        self.settingsView.secondaryWeaponButton.clicked.connect(self.changeFireSecKey())
 
         self.settingsView.soundVolSlider.sliderReleased.connect(self.changeVol)
         self.settingsView.musicVolSlider.sliderReleased.connect(self.changeVol)
@@ -42,7 +45,29 @@ class SettingsViewController(QWidget):
         self.settingsView.musicOnButton.clicked.connect(self.switchMusic)
         self.settingsView.soundOnButton.clicked.connect(self.switchSound)
 
-    def changeKey(self, buttonType):
+    def changeMoveUpKey(self):
+        self.settingsView.moveUpButton.setText("NEU")
+        self.settingsView.moveUpButton.setStyleSheet("Background-Color: blue;")
+        self.setValue(ButtonType.MOVE_UP)
+
+
+
+    def changeMoveDownKey(self):
+        pass
+
+    def changeMoveRightKey(self):
+        pass
+
+    def changeMoveLeftKey(self):
+        pass
+
+    def changeFireMainKey(self):
+        pass
+
+    def changeFireSecKey(self):
+        pass
+
+    def getKey(self, pushButton):
         pass
 
     def switchMusic(self):
@@ -64,7 +89,7 @@ class SettingsViewController(QWidget):
     def changeVol(self):
         pass
 
-    def setValue(self, buttonType):
+    def setInitValue(self, buttonType):
         if buttonType == buttonType.SOUND_ON:
             if self.mainMenuController.signedUser.userSettings.gameSoundOn:
                 self.settingsView.soundOnButton.setText("AN")
@@ -79,20 +104,7 @@ class SettingsViewController(QWidget):
             else:
                 self.settingsView.musicOnButton.setText("AUS")
                 self.settingsView.musicOnButton.setStyleSheet("Background-Color: grey")
-        elif buttonType == buttonType.MOVE_UP:
-            self.settingsView.moveUpButton.setText(self.mainMenuController.signedUser.userSettings.moveUpKey)
-        elif buttonType == buttonType.MOVE_DOWN:
-            self.settingsView.moveDownButton.setText(self.mainMenuController.signedUser.userSettings.moveDownKey)
-        elif buttonType == buttonType.MOVE_LEFT:
-            self.settingsView.moveLeftButton.setText(self.mainMenuController.signedUser.userSettings.moveLeftKey)
-        elif buttonType == buttonType.MOVE_RIGHT:
-            self.settingsView.moveRightButton.setText(self.mainMenuController.signedUser.userSettings.moveRightKey)
-        elif buttonType == buttonType.FIRE_MAIN:
-            self.settingsView.mainWeaponButton.setText(
-                self.mainMenuController.signedUser.userSettings.fireMainWeaponKey)
-        elif buttonType == buttonType.FIRE_SECONDARY:
-            self.settingsView.mainWeaponButton.setText(
-                self.mainMenuController.signedUser.userSettings.fireSecondaryWeaponKey)
+
 
 
 class ButtonType(Enum):

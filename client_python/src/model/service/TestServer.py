1564a07ccef0b654568c1efcbd855ac13c66ca07
DeRocketman <de.rocketman@outlook.com>
2022-01-04 23:01:09 +0100

    ds: socketserver add clients to a list and send chatmassage to all. BUT works not perfect, because a client get a message just after send a chat massage

--- client_python/src/model/service/TestServer.py
+++ client_python/src/model/service/TestServer.py
@@ -1,37 +1,36 @@
 import socket
 from _thread import *
 
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-class SocketServer:
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = "localhost"
+port = 3333
+serverIP = socket.gethostbyname(server)
+threadCount = 0
+playerList = []
+try:
+    serverSocket.bind((server, port))
+except socket.error as e:
+    print(str(e))
 
-    server = "localhost"
-    port = 3333
-    serverIP = socket.gethostbyname(server)
-    threadCount = 0
-    playerList = []
-    try:
-        serverSocket.bind((server, port))
-    except socket.error as e:
-        print(str(e))
+serverSocket.listen(4)
+print("Server ist bereit und heiß auf Connections")
 
-    serverSocket.listen(4)
-    print("Server ist bereit und heiß auf Connections")
 
-    def threaded_client(con):
-        con.send(str.encode("Du hast die Lobby erfolgreich betreten"))
-        while True:
-            msg = con.recv(2048)
-            reply = msg.decode("utf-8")
-            con.send(str.encode(reply))
-            print(reply)
-
+def threaded_client(con):
+    con.send(str.encode("Du hast die Lobby erfolgreich betreten"))
     while True:
-        connection, address = serverSocket.accept()
-        print("Connected to: ", address)
-        start_new_thread(threaded_client, (connection,))
-        threadCount += 1
+        msg = con.recv(2048)
+        reply = msg.decode("utf-8")
+        # con.send(str.encode(reply))
+        print(reply)
+        for player in playerList:
+            player.send(str.encode(reply))
 
 
-if __name__ == '__main__':
-    SoSe = SocketServer()
+while True:
+    connection, address = serverSocket.accept()
+    print("Connected to: ", address)
+    playerList.append(connection)
+    start_new_thread(threaded_client, (connection,))
+    threadCount += 1

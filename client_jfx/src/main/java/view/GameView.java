6b175b33536cd3ad0694a84cba6c0317c3ed04e9
Maximiliami <maximilian.rupprecht@stud.th-luebeck.de>
2022-01-17 21:35:50 +0100

    Aufgeräumt - Factory falsch umgebaut :D - die Erste Reihe Elemente hinzugefügt.

--- client_jfx/src/main/java/view/GameView.java
+++ client_jfx/src/main/java/view/GameView.java
@@ -2,29 +2,20 @@ package view;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
-import de.saxsys.mvvmfx.internal.viewloader.View;
-import javafx.beans.property.ObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import main.ETankApplication;
 import model.game.elements.*;
 import model.game.logic.GamePhysics;
-import model.game.logic.GamePlay;
 import viewmodel.GameViewModel;
-
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
@@ -40,30 +31,6 @@ public class GameView implements FxmlView<GameViewModel>, Initializable {
     private GridPane ground;
     @FXML
     private Pane elementPane;
-    @FXML
-    private StackPane tank1;
-    @FXML
-    private StackPane tank2;
-    @FXML
-    private StackPane tank3;
-    @FXML
-    private StackPane tank4;
-    @FXML
-    private ImageView tank1Hull;
-    @FXML
-    private ImageView tank1Weapon;
-    @FXML
-    private ImageView tank2Hull;
-    @FXML
-    private ImageView tank2Weapon;
-    @FXML
-    private ImageView tank3Hull;
-    @FXML
-    private ImageView tank3Weapon;
-    @FXML
-    private ImageView tank4Hull;
-    @FXML
-    private ImageView tank4Weapon;
 
 
     @Override

0b24ec09e10b9a9514788412fb36754a55ff152b
rocketman <de.rocketman@outlook.com>
2022-01-21 23:43:46 +0100

    MOB: chatMsg to all clients

--- client_jfx/src/main/java/model/service/SocketClient.java
+++ client_jfx/src/main/java/model/service/SocketClient.java
@@ -12,7 +12,7 @@ import java.nio.charset.StandardCharsets;
 public class SocketClient implements Runnable {
     private GameLobbyViewController gameLobbyViewController;
     private GamePlay gamePlay;     //todo: oder in ModelView!
-    private final String hostname = "192.168.178.22";
+    private final String hostname = "217.237.150.205";
     private final int port = 3333;
 
     private final Socket socket;

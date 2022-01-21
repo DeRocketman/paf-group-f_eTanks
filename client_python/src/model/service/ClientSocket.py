668e7b7b07dc2698f064b7911619e9f13e73f387
rocketman <de.rocketman@outlook.com>
2022-01-21 23:44:25 +0100

    MOB: chatMsg to all clients

--- client_python/src/model/service/ClientSocket.py
+++ client_python/src/model/service/ClientSocket.py
@@ -8,7 +8,7 @@ from model.service.Message import Message
 class ClientSocket:
     def __init__(self, newGameViewController):
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "217.237.150.205 "
+        self.host = "217.237.150.205"
         self.port = 3333
 
         self.newGameViewController = newGameViewController

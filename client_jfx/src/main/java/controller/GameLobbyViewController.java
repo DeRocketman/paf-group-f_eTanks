dda87139cf421ced183aec86c69dfe1cdba0d46e
Maximiliami <maximilian.rupprecht@stud.th-luebeck.de>
2022-02-01 21:23:13 +0100

    lobby closed by host will be deleted from lobbyList TEST

--- client_jfx/src/main/java/controller/GameLobbyViewController.java
+++ client_jfx/src/main/java/controller/GameLobbyViewController.java
@@ -460,7 +460,6 @@ public class GameLobbyViewController {
         GameLobby lobby = new GameLobby();
         lobby.setGameLobbyID(msg.getGameLobbyNumber());
         lobby.setSeatCounter(Integer.parseInt(msg.getPayload()));
-        lobbyList.add(lobby);
         fillLobbyTable();
     }
 
